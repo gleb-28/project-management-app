f7e771492b6d6431f039461f2da18d545107bbb5
Gleb28 <iamgleb.g@gmail.com>
2022-11-02 20:22:59 +0300

    init: base

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardsModule } from './boards/boards.module';
+
+@NgModule({
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		CoreModule,
+		AuthModule,
+		BoardsModule,
+		StoreModule.forRoot([]),
+		EffectsModule.forRoot([]),
+		StoreDevtoolsModule.instrument({
+			maxAge: 25,
+			logOnly: environment.production,
+		}),
+	],
+	providers: [],
+	bootstrap: [AppComponent],
+})
+export class AppModule {}
