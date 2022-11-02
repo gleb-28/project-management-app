2c76ab6db1fdab705ca0ab05d72e2ac9a755d890
Gleb28 <iamgleb.g@gmail.com>
2022-11-02 20:22:59 +0300

    refactor: update auth.models and take last commit from develop

--- src/app/models/auth.models.ts
+++ src/app/models/auth.models.ts
@@ -7,14 +7,14 @@ export interface SignInResponse {
   token: string;
 }
 
-export interface SignUpRequest {
-  name: string;
-  login: string;
-  password: string;
-}
-
 export interface SignUpResponse {
   _id: string;
   name: string;
   login: string;
+}
+
+export interface SignUpRequest {
+  name: string;
+  login: string;
+  password: string;
 }
