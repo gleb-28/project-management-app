78247a990706981eb5d110132329749912180ba1
DelfiaHope <valeruanovna94@mail.ru>
2022-11-03 21:47:37 +0300

    feat: add sign-up component

--- src/app/auth/pages/sign-up-page/sign-up-page.component.ts
+++ src/app/auth/pages/sign-up-page/sign-up-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ValidatorPassDate } from '../../validator';
 
 @Component({
 	selector: 'app-sign-up-page',
@@ -7,6 +9,38 @@ import { Component } from '@angular/core';
 })
 export class SignUpPageComponent {
 
+	signUpForm: FormGroup = new FormGroup({
+		name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+		login: new FormControl('', [Validators.required, Validators.minLength(3)]),
+		password: new FormControl('', [
+			Validators.required,
+			Validators.minLength(8),
+			Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{7,}/),
+			ValidatorPassDate.ofRegister,
+			ValidatorPassDate.ofNumber,
+			ValidatorPassDate.ofSymbol,
+		]),
+	});
+
+	signUp() {}
+
+	isRequired(type: string) {
+		const { invalid, touched, errors, dirty } = this.signUpForm.controls[type];
+		return invalid && (touched || dirty) && errors?.['required'];
+	}
+
+	get isPasswordStrong() {
+		const { touched, errors, dirty } = this.signUpForm.controls['password'];
+		return (touched || dirty) && errors?.['pattern'];
+	}
+
+	isTouched(type: string) {
+		return this.signUpForm.controls[type].touched;
+	}
+
+	isValid(error: string, type:string = 'password') {
+		return this.signUpForm.controls[type].errors?.[error];
+	}
 
 
 }
