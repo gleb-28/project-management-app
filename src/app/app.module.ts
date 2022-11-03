6262c180db04743aa09c21963bb17a3a9992a362
Gleb28 <iamgleb.g@gmail.com>
2022-11-04 02:33:56 +0300

    feat: core components(welcome-page, 404-page, header, footer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
@@ -15,6 +16,7 @@ import { BoardsModule } from './boards/boards.module';
 	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
 		CoreModule,
 		AuthModule,
