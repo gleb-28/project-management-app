a56b012741f68f4780c9d8f823a9fa4734e33c45
Gleb28 <iamgleb.g@gmail.com>
2022-11-06 19:48:38 +0300

    refactor: added team constant and used it in the template

---
+++ src/app/constants/team.constant.ts
@@ -0,0 +1,27 @@
+export const team = [
+	{
+		fullName: 'Artsiom Fando',
+		role: 'mentor',
+		gitHubLink: 'https://github.com/artsiomfando/',
+	},
+	{
+		fullName: 'Gleb Gruzdov',
+		role: 'team lead',
+		gitHubLink: 'https://github.com/gleb-28',
+	},
+	{
+		fullName: 'Yan Chorny',
+		role: 'team member',
+		gitHubLink: 'https://github.com/janChorny',
+	},
+	{
+		fullName: 'Nadya Rudenok',
+		role: 'team member',
+		gitHubLink: 'https://github.com/delfiahope',
+	},
+	{
+		fullName: 'Anatoliy Aliaksandrau',
+		role: 'team member',
+		gitHubLink: 'https://github.com/AnAtoliyAK',
+	},
+];
