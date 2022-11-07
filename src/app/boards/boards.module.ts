c05f5433fd5090b8cc1fb36ea225133245f0d2b5
Gleb28 <iamgleb.g@gmail.com>
2022-11-07 23:46:14 +0300

    feat: added filterByPipe

--- src/app/boards/boards.module.ts
+++ src/app/boards/boards.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FilterByPipe } from './pipes/filter-by-pipe/filter-by.pipe';
 
 @NgModule({
-	declarations: [],
+	declarations: [FilterByPipe],
 	imports: [CommonModule],
+	providers: [FilterByPipe],
 })
 export class BoardsModule {}
