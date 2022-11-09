4b5221df7f7e8e2761a24c33b98ce06f7fb8fb39
janChorny <janchorny@tut.by>
2022-11-09 15:56:19 +0300

    feat: add tasks methods to board interface

--- src/app/boards/services/board.service.ts
+++ src/app/boards/services/board.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ColumnRequest, ColumnResponse } from 'src/app/models/column.models';
+import { TaskRequest, TaskResponse } from 'src/app/models/task.models';
 
 
 @Injectable({
@@ -44,6 +45,42 @@ export class BoardService {
 
 	public createSetOfColumns(columns: [ColumnRequest]): Observable<ColumnResponse[]> {
 		return this.http.post<ColumnResponse[]>('/columnsSet', columns);
+	}
+
+	public getTasksInColumns(boardId: string, columnId: string): Observable<TaskResponse[]> {
+		return this.http.get<TaskResponse[]>(`/boards/${boardId}/columns/${columnId}/tasks`);
+	}
+
+	public createTask(boardId: string, columnId: string, taskData: TaskRequest): Observable<TaskResponse> {
+		return this.http.post<TaskResponse>(`/boards/${boardId}/columns/${columnId}/tasks`, taskData);
+	}
+
+	public getTaskById(boardId: string, columnId: string, taskId: string): Observable<TaskResponse> {
+		return this.http.get<TaskResponse>(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`);
+	}
+
+	public updateTaskById(boardId: string, columnId: string, taskId: string, taskData: TaskRequest): Observable<TaskResponse> {
+		return this.http.put<TaskResponse>(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`, taskData);
+	}
+
+	public deleteTaskById(boardId: string, columnId: string, taskId: string): Observable<TaskResponse> {
+		return this.http.delete<TaskResponse>(`/boards/${boardId}/columns/${columnId}/tasks/${taskId}`);
+	}
+
+	public getTaskByIdUserIdOrSearch(ids: [string], userId: string, search: string): Observable<TaskResponse[]> {
+		const params = new HttpParams()
+			.set('ids', JSON.stringify(ids))
+			.set('userId', userId)
+			.set('search', search);
+		return this.http.get<TaskResponse[]>('/tasksSet', { params: params });
+	}
+
+	public updateSetOfTasks(tasks: TaskRequest[]): Observable<TaskResponse[]> {
+		return this.http.patch<TaskResponse[]>('/tasksSet', tasks);
+	}
+
+	public getTaskByBoardId(boardId: string): Observable<TaskResponse[]> {
+		return this.http.get<TaskResponse[]>(`/tasksSet/${boardId}`);
 	}
 
 }
