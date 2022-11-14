0b35e3678eecc74d6d1e9f5f28963d3583ba783d
Gleb28 <iamgleb.g@gmail.com>
2022-11-14 21:48:17 +0300

    fix: changed auth guard and fixed bugs

--- src/app/auth/guards/auth-guard/auth.guard.ts
+++ src/app/auth/guards/auth-guard/auth.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Router, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { selectIsLogged } from '../../../store/selectors/user-selector/user.selector';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class AuthGuard implements CanLoad {
-	constructor(private router: Router) {}
+export class AuthGuard implements CanLoad, CanActivate {
+	isLogged$ = this.store.select(selectIsLogged);
 
-	canLoad(): boolean | UrlTree {
-		const isLogged = false; //TODO: get stream from store
-		return isLogged ? true : this.router.parseUrl('welcome');
+	constructor(private router: Router, private store: Store) {}
+
+	canActivate(): Observable<boolean | UrlTree> {
+		return this.checkAuth();
+	}
+
+	canLoad(): Observable<boolean | UrlTree> {
+		return this.checkAuth();
+	}
+
+	private checkAuth(): Observable<boolean | UrlTree> {
+		return this.isLogged$.pipe(map((isLogged) => (isLogged ? true : this.router.parseUrl('welcome'))));
 	}
 }
--- src/app/auth/guards/logged-auth-guard/logged-auth.guard.ts
+++ src/app/auth/guards/logged-auth-guard/logged-auth.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router, UrlTree } from '@angular/router';
+import { CanActivate, CanLoad, Router, UrlTree } from '@angular/router';
+import { selectIsLogged } from '../../../store/selectors/user-selector/user.selector';
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
 	providedIn: 'root',
 })
-export class LoggedAuthGuard implements CanLoad {
-	constructor(private router: Router) {}
+export class LoggedAuthGuard implements CanLoad, CanActivate {
+	isLogged$ = this.store.select(selectIsLogged);
 
-	canLoad(): boolean | UrlTree {
-		const isLogged = false; //TODO: get stream from store
-		return !isLogged ? true : this.router.parseUrl('');
+	constructor(private router: Router, private store: Store) {}
+
+	canActivate(): Observable<boolean | UrlTree> {
+		return this.checkAuth();
+	}
+
+	canLoad(): Observable<boolean | UrlTree> {
+		return this.checkAuth();
+	}
+
+	private checkAuth(): Observable<boolean | UrlTree> {
+		return this.isLogged$.pipe(map((isLogged) => (!isLogged ? true : this.router.parseUrl(''))));
 	}
 }
