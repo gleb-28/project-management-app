e4e27c55cde5646e7f1fa0068ce4e05541bca2b8
Gleb28 <iamgleb.g@gmail.com>
2022-11-25 18:34:29 +0300

    feat: new design + refactor

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -9,12 +9,12 @@ import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { AccordionModule } from 'primeng/accordion';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkWithHref } from '@angular/router';
 import { DialogModule } from 'primeng/dialog';
 import { InputTextModule } from 'primeng/inputtext';
 import { TranslateModule } from '@ngx-translate/core';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { PreloaderComponent } from './components/preloader/preloader/preloader.component';
+import { PreloaderComponent } from './components/preloader/preloader.component';
 import { ErrorToastComponent } from './components/error-toast/error-toast.component';
 import { ToastModule } from 'primeng/toast';
 
@@ -33,6 +33,7 @@ const PrimeNgModules = [
 	AccordionModule,
 	ProgressSpinnerModule,
 	ToastModule,
+	InputTextModule,
 ];
 
 @NgModule({
@@ -44,7 +45,7 @@ const PrimeNgModules = [
 		TranslateModule,
 		DialogModule,
 		ReactiveFormsModule,
-		InputTextModule,
+		RouterLinkWithHref,
 		...PrimeNgModules,
 	],
 	exports: [...Components],
